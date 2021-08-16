b352c0f66eaacb47c04c48b176d6ad136651e52e
Filadelfo <filadelfo.braz@gmail.com>
2021-08-16 13:10:18 +0200

    Feat: Add tabs for import products and see requests

--- react/components/ProductTranslation/ProductImportModal.tsx
+++ react/components/ProductTranslation/ProductImportModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { ModalDialog, ButtonPlain, Dropzone } from 'vtex.styleguide'
+import { ModalDialog, ButtonPlain, Dropzone, Tabs, Tab } from 'vtex.styleguide'
 
 import { sanitizeImportJSON, parseXLSToJSON, parseJSONToXLS } from '../../utils'
 import WarningAndErrorsImportModal from '../WarningAndErrorsImportModal'
@@ -29,6 +29,8 @@ const ProductImportModal = ({
   const [translationsAdj, setTranslationsAdj] = useState<
     Array<Record<EntryHeaders<Product>, string>>
   >([])
+
+  const [tabSelected, setTabSelected] = useState<1 | 2>(1)
 
   const handleFile = async (files: FileList) => {
     if (loading) {
@@ -122,46 +124,73 @@ const ProductImportModal = ({
         cleanErrors()
       }}
     >
+      <h3>Import Category Translations</h3>
       <div>
-        <h3>Import Category Translations</h3>
-        <ButtonPlain onClick={createModel}>Download xlsx model</ButtonPlain>
-        <div className="mt2">
-          <Dropzone
-            accept=".xlsx"
-            onDropAccepted={handleFile}
-            onFileReset={handleReset}
+        <Tabs>
+          <Tab
+            label="Import"
+            active={tabSelected === 1}
+            onClick={() => {
+              setTabSelected(1)
+            }}
           >
-            <div className="pt7">
-              <span className="f4">Drop here your XLSX or </span>
-              <span className="f4 c-link" style={{ cursor: 'pointer' }}>
-                choose a file
-              </span>
+            <div>
+              <div className="mv4">
+                <ButtonPlain onClick={createModel}>
+                  Download xlsx model
+                </ButtonPlain>
+              </div>
+              <div>
+                <Dropzone
+                  accept=".xlsx"
+                  onDropAccepted={handleFile}
+                  onFileReset={handleReset}
+                >
+                  <div className="pt7">
+                    <span className="f4">Drop here your XLSX or </span>
+                    <span className="f4 c-link" style={{ cursor: 'pointer' }}>
+                      choose a file
+                    </span>
+                  </div>
+                </Dropzone>
+              </div>
+              {errorParsingFile ? (
+                <p className="c-danger i f7">{errorParsingFile}</p>
+              ) : null}
             </div>
-          </Dropzone>
-        </div>
-        {errorParsingFile ? (
-          <p className="c-danger i f7">{errorParsingFile}</p>
-        ) : null}
+            <ul>
+              {originalFile.length ? (
+                <li>{originalFile?.length} total entries</li>
+              ) : null}
+              {validtionWarnings.length ? (
+                <li>
+                  <ButtonPlain onClick={() => setWarningModal(true)}>
+                    {validtionWarnings.length} warnings.
+                  </ButtonPlain>
+                </li>
+              ) : null}
+              {validtionErrors.length ? (
+                <li>
+                  <ButtonPlain
+                    variation="danger"
+                    onClick={() => setErrorModal(true)}
+                  >
+                    {validtionErrors.length} errors. The entries will be
+                    ignored.
+                  </ButtonPlain>
+                </li>
+              ) : null}
+            </ul>
+          </Tab>
+          <Tab
+            label="Requests"
+            active={tabSelected === 2}
+            onClick={() => setTabSelected(2)}
+          >
+            <div>Request list</div>
+          </Tab>
+        </Tabs>
       </div>
-      <ul>
-        {originalFile.length ? (
-          <li>{originalFile?.length} total entries</li>
-        ) : null}
-        {validtionWarnings.length ? (
-          <li>
-            <ButtonPlain onClick={() => setWarningModal(true)}>
-              {validtionWarnings.length} warnings.
-            </ButtonPlain>
-          </li>
-        ) : null}
-        {validtionErrors.length ? (
-          <li>
-            <ButtonPlain variation="danger" onClick={() => setErrorModal(true)}>
-              {validtionErrors.length} errors. The entries will be ignored.
-            </ButtonPlain>
-          </li>
-        ) : null}
-      </ul>
       <WarningAndErrorsImportModal
         isOpen={warningModal}
         modalName="Warning Modal"
